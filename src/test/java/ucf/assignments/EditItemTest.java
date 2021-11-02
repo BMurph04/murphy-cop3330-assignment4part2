d1ff1b0ece91d902f777d23e2b46a65d3f31e56f
Brendon <murph4012@gmail.com>
2021-11-01 22:25:37 -0400

    Third Commit

---
+++ src/test/java/ucf/assignments/EditItemTest.java
@@ -0,0 +1,38 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EditItemTest {
+
+    @Test
+    void applyToItem() {
+        //Tests that new item's name, due date, description are all correct
+    }
+
+    @Test
+    void editItemName() {
+        //Tests that new item name was updated
+    }
+
+    @Test
+    void editItemDesc() {
+        //Tests that new item desc was updated
+    }
+
+    @Test
+    void editItemDate() {
+        //Tests that new item date was updated
+    }
+
+    @Test
+    void editItemComplete() {
+        //Tests that new item completion was updated
+    }
+
+    @Test
+    void removeItem() {
+        //Tests that item was removed
+    }
+}
---
+++ src/test/java/ucf/assignments/FilterListTest.java
@@ -0,0 +1,23 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterListTest {
+
+    @Test
+    void showCompleteItems() {
+        //Tests that items shown are those marked as complete
+    }
+
+    @Test
+    void showIncompleteItems() {
+        //Tests that items shown are those not marked as complete
+    }
+
+    @Test
+    void showAllItems() {
+        //Tests that all items are shown
+    }
+}
